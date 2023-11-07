1d2ac44a574db48fa5b048e0006e83c2d33dd3f0
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2023-11-07 21:11:56 +0800

    Initial commit

---
+++ pe.mk
@@ -0,0 +1,14 @@
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/BoardConfigSoong.mk)
+$(call inherit-product, device/custom/sepolicy/common/sepolicy.mk)
+-include vendor/aosp/build/core/config.mk
+
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_pe/thirteen/ota.json
