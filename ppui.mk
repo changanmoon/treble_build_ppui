1c9deaf5e5e8ff1c49c3a155d4cea98e38a764ab
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2023-11-07 21:28:50 +0800

    Update and rename pe.mk to ppui.mk

--- ppui.mk
+++ ppui.mk
@@ -11,4 +11,4 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_CALL_RECORDING := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_pe/thirteen/ota.json
+    ro.system.ota.json_url=https://raw.githubusercontent.com/changanmoon/treble_build_ppui/tiramisu/ota.json
